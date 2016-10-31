71d646b1ed9b0195cf943475ff42d631e05737db
Alexandr Romanishin <sasha.sirelon@gmail.com>
2016-10-31 11:51:31 +0200

    add picasso dependncy and create extension for load image by url

--- app/src/main/java/com/sirelon/marsroverphotos/Extensions.kt
+++ app/src/main/java/com/sirelon/marsroverphotos/Extensions.kt
@@ -2,15 +2,25 @@
 
 package com.sirelon.marsroverphotos
 
+import android.text.TextUtils
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import com.squareup.picasso.Picasso
 
 /**
  * @author romanishin
  * @since 31.10.16 on 11:26
  */
 
-fun ViewGroup.inflate(layoutId: Int, attachToRoot: Boolean = false) : View{
+fun ViewGroup.inflate(layoutId: Int, attachToRoot: Boolean = false): View {
     return LayoutInflater.from(context).inflate(layoutId, this, attachToRoot)
+}
+
+fun ImageView.loadImage(imageUrl: String) {
+    if (TextUtils.isEmpty(imageUrl))
+        this.setImageResource(R.drawable.img_placeholder)
+    else
+        Picasso.with(context).load(imageUrl).into(this)
 }
--- app/src/main/java/com/sirelon/marsroverphotos/models/MarsPhoto.kt
+++ app/src/main/java/com/sirelon/marsroverphotos/models/MarsPhoto.kt
@@ -6,7 +6,7 @@ import com.sirelon.marsroverphotos.adapter.AdapterConstants
  * @author romanishin
  * @since 31.10.16 on 11:19
  */
-class MarsPhoto(val name: String, val imageUrl: String) : ViewType {
+data class MarsPhoto(var name: String, var imageUrl: String) : ViewType {
 
     override fun getViewType(): Int = AdapterConstants.MARS_PHOTO
 
