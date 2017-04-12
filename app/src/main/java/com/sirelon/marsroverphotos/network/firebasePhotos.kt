28a4b62798a61ada10930c49dc33317e1da8e9cf
Alexandr Romanishyn <alexandr.romanishin@srost-studio.com>
2017-04-12 19:07:52 +0300

    FirebasePhoto

--- app/src/main/java/com/sirelon/marsroverphotos/network/firebasePhotos.kt
+++ app/src/main/java/com/sirelon/marsroverphotos/network/firebasePhotos.kt
@@ -2,6 +2,7 @@ package com.sirelon.marsroverphotos.network
 
 import com.google.firebase.database.FirebaseDatabase
 import com.sirelon.marsroverphotos.extensions.setValueObservable
+import com.sirelon.marsroverphotos.models.FirebasePhoto
 import com.sirelon.marsroverphotos.models.MarsPhoto
 import io.reactivex.Observable
 
@@ -12,14 +13,8 @@ object firebasePhotos {
 
     fun addMarsPhoto(photo: MarsPhoto): Observable<MarsPhoto> {
         val ref = FirebaseDatabase.getInstance().getReference("${FirebaseConstants.PHOTOS_TABLE}/${photo.id}")
-        return ref.setValueObservable(photo.toFireBaseMap()).map { photo }
+        return ref.setValueObservable(photo.toFireBase()).map { photo }
     }
 
-    private fun MarsPhoto.toFireBaseMap(): MutableMap<String, Any> {
-        val map = mutableMapOf<String, Any>()
-        map.put(FirebaseConstants.PHOTOS_EARTH_DATE, this.earthDate)
-        map.put(FirebaseConstants.PHOTOS_IMAGE, this.imageUrl)
-        map.put(FirebaseConstants.PHOTO_SOL, this.sol)
-        return map
-    }
+    private fun MarsPhoto.toFireBase(): FirebasePhoto = FirebasePhoto(this)
 }
