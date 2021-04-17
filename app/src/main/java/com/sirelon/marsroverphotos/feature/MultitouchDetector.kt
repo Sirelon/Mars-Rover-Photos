2fe0ab8d4a74720d9abe284150eaea6cb762994e
Aleksandr Romanishyn <aleksandr.romanishyn@appflame.com>
2021-04-18 00:10:59 +0300

    Seems, that i figure out how to make it scallable and scrollable! part 2

--- app/src/main/java/com/sirelon/marsroverphotos/feature/MultitouchDetector.kt
+++ app/src/main/java/com/sirelon/marsroverphotos/feature/MultitouchDetector.kt
@@ -12,20 +12,30 @@ import androidx.compose.foundation.gestures.detectTransformGestures
 import androidx.compose.foundation.gestures.forEachGesture
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.offset
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.input.pointer.consumeAllChanges
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.input.pointer.positionChangeConsumed
 import androidx.compose.ui.input.pointer.positionChanged
+import androidx.compose.ui.layout.onGloballyPositioned
+import androidx.compose.ui.layout.positionInParent
+import androidx.compose.ui.unit.IntOffset
+import androidx.compose.ui.unit.IntSize
 import androidx.compose.ui.util.fastAny
 import androidx.compose.ui.util.fastForEach
+import com.sirelon.marsroverphotos.feature.images.FullScreenImage
 import kotlin.math.PI
 import kotlin.math.abs
-import kotlin.math.absoluteValue
+import kotlin.math.roundToInt
 import kotlin.math.sqrt
 
 /**
@@ -96,131 +106,133 @@ fun MultitouchDetectorNorm(
 @Composable
 fun MultitouchDetector(
     modifier: Modifier,
-    enabled: Boolean,
-    callback: (zoom: Float, offsetX: Float, offsetY: Float) -> Unit
+    content: @Composable () -> Unit
 ) {
 //    val matrix by remember { mutableStateOf(Matrix()) }
-//    var angle by remember { mutableStateOf(0f) }
-//    var zoom by remember { mutableStateOf(1f) }
-//    var offsetX by remember { mutableStateOf(0f) }
-//    var offsetY by remember { mutableStateOf(0f) }
 
-    val scale = remember { mutableStateOf(1f) }
-    val translate = remember { mutableStateOf(Offset(0f, 0f)) }
+    var zoomToChange by remember { mutableStateOf(1f) }
+    var offsetX by remember { mutableStateOf(0f) }
+    var offsetY by remember { mutableStateOf(0f) }
+//    var size by remember { mutableStateOf(IntSize(0, 0)) }
+    var position by remember { mutableStateOf(Offset.Zero) }
+    var parentSize by remember { mutableStateOf(IntSize(0, 0)) }
 
     Box(
         modifier
             .fillMaxSize()
 //            .background(Color.Green)
             .pointerInput(Unit) {
-                if (enabled) {
-                    forEachGesture {
-                        awaitPointerEventScope {
-                            var rotation = 0f
-                            var zoom = 1f
-                            var pan = Offset.Zero
-                            var pastTouchSlop = false
-                            val touchSlop = viewConfiguration.touchSlop
-                            var lockedToPanZoom = false
+                forEachGesture {
+                    awaitPointerEventScope {
+                        var rotation = 0f
+                        var zoom = 1f
+                        var pan = Offset.Zero
+                        var pastTouchSlop = false
+                        val touchSlop = viewConfiguration.touchSlop
+                        var lockedToPanZoom = false
 
-                            awaitFirstDown(requireUnconsumed = false)
-                            do {
-                                val event = awaitPointerEvent()
-                                val canceled = event.changes.fastAny { it.positionChangeConsumed() }
-                                if (!canceled) {
-                                    val zoomChange = event.calculateZoom()
-                                    val rotationChange = event.calculateRotation()
-                                    val panChange = event.calculatePan()
+                        awaitFirstDown(requireUnconsumed = false)
+                        do {
+                            val event = awaitPointerEvent()
+                            val canceled = event.changes.fastAny { it.positionChangeConsumed() }
+                            if (!canceled) {
+                                val zoomChange = event.calculateZoom()
+                                val rotationChange = event.calculateRotation()
+                                val panChange = event.calculatePan()
 
-                                    if (!pastTouchSlop) {
-                                        zoom *= zoomChange
-                                        rotation += rotationChange
-                                        pan += panChange
+                                if (!pastTouchSlop) {
+                                    zoom *= zoomChange
+                                    rotation += rotationChange
+                                    pan += panChange
 
-                                        val centroidSize =
-                                            event.calculateCentroidSize(useCurrent = false)
-                                        val zoomMotion = abs(1 - zoom) * centroidSize
-                                        val rotationMotion =
-                                            abs(rotation * PI.toFloat() * centroidSize / 180f)
-                                        val panMotion = pan.getDistance()
+                                    val centroidSize =
+                                        event.calculateCentroidSize(useCurrent = false)
+                                    val zoomMotion = abs(1 - zoom) * centroidSize
+                                    val rotationMotion =
+                                        abs(rotation * PI.toFloat() * centroidSize / 180f)
+                                    val panMotion = pan.getDistance()
 
-                                        if (zoomMotion > touchSlop ||
-                                            rotationMotion > touchSlop ||
-                                            panMotion > touchSlop
-                                        ) {
-                                            pastTouchSlop = true
-                                            lockedToPanZoom = false && rotationMotion < touchSlop
-                                        }
-                                    }
-
-                                    if (pastTouchSlop) {
-                                        val centroid = event.calculateCentroid(useCurrent = false)
-                                        val effectiveRotation =
-                                            if (lockedToPanZoom) 0f else rotationChange
-                                        if (effectiveRotation != 0f ||
-                                            zoomChange != 1f ||
-                                            panChange != Offset.Zero
-                                        ) {
-//                                            onGesture(centroid, panChange, zoomChange, effectiveRotation, callback)
-
-                                            val anchorX = centroid.x - size.width / 2f
-                                            val anchorY = centroid.y - size.height / 2f
-                                            val matrix = Matrix()
-                                            matrix.postRotate(effectiveRotation, anchorX, anchorY)
-                                            matrix.postScale(
-                                                zoomChange,
-                                                zoomChange,
-                                                anchorX,
-                                                anchorY
-                                            )
-                                            matrix.postTranslate(pan.x, pan.y)
-
-                                            val v = FloatArray(9)
-                                            matrix.getValues(v)
-                                            val scaleX = v[Matrix.MSCALE_X]
-                                            val skewY = v[Matrix.MSKEW_Y]
-//                                            val offsetX = v[Matrix.MTRANS_X]
-//                                            val offsetY = v[Matrix.MTRANS_Y]
-                                            val offsetX = panChange.x
-                                            val offsetY = panChange.y
-                                            val zoom = sqrt(scaleX * scaleX + skewY * skewY)
-                                            callback(zoom, offsetX, offsetY)
-
-
-                                            if (offsetX.absoluteValue < 7f) {
-                                                Log.i("Sirelon1", "OFFSET ! $offsetX")
-                                                event.changes.fastForEach {
-                                                    if (it.positionChanged()) {
-                                                        it.consumeAllChanges()
-                                                    }
-                                                }
-                                            }
-                                        }
-//                                        event.changes.fastForEach {
-//                                            if (it.positionChanged()) {
-//                                                it.consumeAllChanges()
-//                                            }
-//                                        }
+                                    if (zoomMotion > touchSlop ||
+                                        rotationMotion > touchSlop ||
+                                        panMotion > touchSlop
+                                    ) {
+                                        pastTouchSlop = true
+                                        lockedToPanZoom = false && rotationMotion < touchSlop
                                     }
                                 }
-                            } while (!canceled && event.changes.fastAny { it.pressed })
-                        }
+
+                                if (pastTouchSlop) {
+                                    val centroid = event.calculateCentroid(useCurrent = false)
+                                    val effectiveRotation =
+                                        if (lockedToPanZoom) 0f else rotationChange
+                                    if (effectiveRotation != 0f ||
+                                        zoomChange != 1f ||
+                                        panChange != Offset.Zero
+                                    ) {
+//                                            onGesture(centroid, panChange, zoomChange, effectiveRotation, callback)
+
+                                        val anchorX = centroid.x - size.width / 2f
+                                        val anchorY = centroid.y - size.height / 2f
+                                        val matrix = Matrix()
+                                        matrix.postRotate(effectiveRotation, anchorX, anchorY)
+                                        matrix.postScale(
+                                            zoomChange,
+                                            zoomChange,
+                                            anchorX,
+                                            anchorY
+                                        )
+                                        matrix.postTranslate(pan.x, pan.y)
+
+                                        val v = FloatArray(9)
+                                        matrix.getValues(v)
+                                        val scaleX = v[Matrix.MSCALE_X]
+                                        val skewY = v[Matrix.MSKEW_Y]
+//                                            val offsetX = v[Matrix.MTRANS_X]
+//                                            val offsetY = v[Matrix.MTRANS_Y]
+                                        val offsetXVal = panChange.x
+                                        val offsetYVal = panChange.y
+                                        val zoomVal = sqrt(scaleX * scaleX + skewY * skewY)
+//                                            callback(zoom, offsetX, offsetY)
+
+                                        zoomToChange *= zoomVal
+                                        offsetX += offsetXVal
+                                        offsetY += offsetYVal
+
+//                                            if (offsetX.absoluteValue < 7f) {
+//                                                Log.i("Sirelon1", "OFFSET ! $offsetX")
+//                                                event.changes.fastForEach {
+//                                                    if (it.positionChanged()) {
+//                                                        it.consumeAllChanges()
+//                                                    }
+//                                                }
+//                                            }
+                                    }
+                                    event.changes.fastForEach {
+                                        if (it.positionChanged()) {
+                                            it.consumeAllChanges()
+                                        }
+                                    }
+                                }
+                            }
+                        } while (!canceled && event.changes.fastAny { it.pressed })
                     }
                 }
             }
     ) {
-//        Box(
-//            Modifier
-//                .offset { IntOffset(offsetX.roundToInt(), offsetY.roundToInt()) }
-//                .graphicsLayer(
-//                    scaleX = zoom,
-//                    scaleY = zoom,
-////                    rotationZ = angle
-//                )
-//                .fillMaxSize()
-//        ) {
-//            content()
-//        }
+        val intOffset = IntOffset(offsetX.roundToInt(), offsetY.roundToInt())
+        FullScreenImage(
+            Modifier
+                .align(Alignment.Center)
+                .offset { intOffset }
+                .graphicsLayer(scaleX = zoomToChange, scaleY = zoomToChange)
+                .onGloballyPositioned {
+//                    size = it.size
+                    position = it.positionInParent()
+                    parentSize = it.parentCoordinates?.size!!
+                }
+                .fillMaxSize(),
+            imageUrl = "https://redstapler.co/wp-content/uploads/2019/05/3d-photo-from-image-1.jpg"
+        )
     }
 
 }
--- app/src/main/java/com/sirelon/marsroverphotos/feature/images/ImagesPager.kt
+++ app/src/main/java/com/sirelon/marsroverphotos/feature/images/ImagesPager.kt
@@ -2,29 +2,19 @@ package com.sirelon.marsroverphotos.feature.images
 
 import android.util.Log
 import androidx.compose.foundation.background
-import androidx.compose.foundation.gestures.scrollBy
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.offset
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.graphicsLayer
-import androidx.compose.ui.layout.onGloballyPositioned
-import androidx.compose.ui.layout.positionInParent
-import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.IntSize
 import com.google.accompanist.coil.CoilImage
 import com.google.accompanist.imageloading.ImageLoadState
@@ -33,11 +23,7 @@ import com.google.accompanist.pager.ExperimentalPagerApi
 import com.google.accompanist.pager.HorizontalPager
 import com.google.accompanist.pager.rememberPagerState
 import com.sirelon.marsroverphotos.feature.MultitouchDetector
-import com.sirelon.marsroverphotos.feature.MultitouchDetectorNorm
 import com.sirelon.marsroverphotos.storage.MarsImage
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
-import kotlin.math.roundToInt
 
 /**
  * Created on 13.04.2021 22:52 for Mars-Rover-Photos.
@@ -71,82 +57,45 @@ fun ImagesPager(viewModel: ImageViewModel = androidx.lifecycle.viewmodel.compose
             .fillMaxSize()
             .background(Color.Gray)
     ) { page ->
-
-        Log.d("Sirelon", "page $page")
-        val scope = rememberCoroutineScope()
-
         // Our page content
         val marsImage = images[page]
 
         Box {
-            var zoom by remember { mutableStateOf(1f) }
-            var offsetX by remember { mutableStateOf(0f) }
-            var offsetY by remember { mutableStateOf(0f) }
-            var size by remember { mutableStateOf(IntSize(0, 0)) }
-            var position by remember { mutableStateOf(Offset.Zero) }
-            var parentSize by remember { mutableStateOf(IntSize(0, 0)) }
 //            if (pagerState.isScrollInProgress) {
 //                zoom = 1f
 //                offsetX = 0f
 //                offsetY = 0f
 //            }
 
-            val intOffset = IntOffset(offsetX.roundToInt(), offsetY.roundToInt())
-            FullScreenImage(
-                modifier = Modifier
-                    .align(Alignment.Center)
-                    .offset { intOffset }
-                    .graphicsLayer(scaleX = zoom, scaleY = zoom)
-                    .onGloballyPositioned {
-                        size = it.size
-                        position = it.positionInParent()
-                        parentSize = it.parentCoordinates?.size!! ?: it.size * 2
-                    }
-                    .fillMaxSize(),
-                marsImage = marsImage)
-
-            Column {
-                val enabled = !pagerState.isScrollInProgress && offsetX >= 0f
-                val intSize = size * zoom.roundToInt()
-                Log.d(
-                    "Sirelon",
-                    "PositionX = $offsetX and position $position"
-                )
-                Spacer(modifier = Modifier.weight(1f))
-                MultitouchDetector(
-                    modifier = Modifier
-                        .weight(2f)
-                        .background(Color.Red.copy(alpha = 0.5F)),
-                    enabled = true
-
-                ) { z, x, y ->
-                    zoom *= z
-                    val offX = position.x + (size.width * zoom)
-                    Log.w("Sirelon1", "OFfX ${pagerState.currentPageOffset}")
-                    if (x > 0) {
-                        if (position.x < 0)
-                            offsetX += x
-                    } else if (offX > parentSize.width) {
-                        offsetX += x
-                    } else {
-//                        scope.launch {
-//
-//                            pagerState.scrollBy(x)
-//                        }
-                    }
-                    offsetY += y
-                }
-                Spacer(modifier = Modifier.weight(1f))
+            MultitouchDetector(modifier = Modifier) {
+//                FullScreenImage(
+//                    modifier = Modifier.fillMaxSize(),
+//                    imageUrl = marsImage.imageUrl
+//                )
             }
         }
     }
 }
 
+
+//zoom *= z
+//val offX = position.x + (size.width * zoom)
+//if (x > 0) {
+//    if (position.x < 0)
+//        offsetX += x
+//} else if (offX > parentSize.width) {
+//    offsetX += x
+//} else {
+////                        scope.launch {
+////
+////                            pagerState.scrollBy(x)
+////                        }
+//}
 @Composable
-private fun FullScreenImage(modifier: Modifier, marsImage: MarsImage) {
+fun FullScreenImage(modifier: Modifier, imageUrl: String) {
     CoilImage(
         modifier = modifier,
-        data = marsImage.imageUrl
+        data = imageUrl
     ) { imageState ->
         when (imageState) {
             is ImageLoadState.Success -> {
@@ -166,3 +115,5 @@ private fun FullScreenImage(modifier: Modifier, marsImage: MarsImage) {
         }
     }
 }
+
+//offsetY += y
