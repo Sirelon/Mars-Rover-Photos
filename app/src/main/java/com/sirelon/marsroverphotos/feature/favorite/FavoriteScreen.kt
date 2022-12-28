523e090a10afa9625f6cdb7602731851f75be95c
sirelon <sasha.sirelon@gmail.com>
2022-12-29 01:59:23 +0200

    Implemented change grid view to list view on Favorite screen.

--- app/src/main/java/com/sirelon/marsroverphotos/feature/favorite/FavoriteScreen.kt
+++ app/src/main/java/com/sirelon/marsroverphotos/feature/favorite/FavoriteScreen.kt
@@ -1,18 +1,28 @@
 package com.sirelon.marsroverphotos.feature.favorite
 
-import android.util.Log
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.grid.GridCells
-import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
-import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.foundation.lazy.staggeredgrid.LazyVerticalStaggeredGrid
+import androidx.compose.foundation.lazy.staggeredgrid.StaggeredGridCells
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.LinearProgressIndicator
 import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.GridView
+import androidx.compose.material.icons.filled.ViewList
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -22,7 +32,6 @@ import androidx.navigation.NavController
 import androidx.paging.LoadState
 import androidx.paging.compose.LazyPagingItems
 import androidx.paging.compose.collectAsLazyPagingItems
-import androidx.paging.compose.items
 import com.sirelon.marsroverphotos.R
 import com.sirelon.marsroverphotos.feature.MarsImageComposable
 import com.sirelon.marsroverphotos.feature.navigateToImages
@@ -83,6 +92,7 @@ fun PopularScreen(
     )
 }
 
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun FavoritePhotosContent(
     modifier: Modifier,
@@ -92,65 +102,69 @@ fun FavoritePhotosContent(
     onFavoriteClick: (image: MarsImage) -> Unit,
     emptyContent: @Composable () -> Unit
 ) {
-    // LazyVerticalStaggeredGrid
-    if (false) {
-        LazyVerticalGrid(columns = GridCells.Fixed(2), content = {
-            items(items.itemCount) {
-                val image = items[it]
-                if (image != null) {
-                    MarsImageComposable(
-                        marsImage = image,
-                        onClick = { onItemClick(image) },
-                        onFavoriteClick = { onFavoriteClick(image) })
-                } else
-                    Image(
-                        painter = painterResource(id = R.drawable.img_placeholder),
-                        contentDescription = ""
-                    )
-            }
 
-        })
-        return
+    var gridView by remember {
+        mutableStateOf(false)
     }
 
-    LazyColumn(modifier = modifier, contentPadding = PaddingValues(16.dp), content = {
-
-        item { TopAppBar(title = { Text(text = title) }) }
-
-        Log.d("Sirelon", "FavoritePhotosContent() called ${items.loadState}")
-        if (items.loadState.refresh == LoadState.Loading) {
-            item {
-                CenteredProgress()
+    Column(modifier = Modifier.fillMaxSize()) {
+        TopAppBar(
+            title = { Text(text = title) },
+            actions = {
+                IconButton(onClick = { gridView = !gridView }) {
+                    if (gridView) {
+                        Icon(
+                            imageVector = Icons.Default.ViewList,
+                            contentDescription = "Change to List View",
+                        )
+                    } else {
+                        Icon(
+                            imageVector = Icons.Default.GridView,
+                            contentDescription = "Change to Grid View",
+                        )
+                    }
+                }
             }
+        )
+
+        if (items.loadState.refresh == LoadState.Loading) {
+            CenteredProgress()
         }
 
         if (items.loadState.append.endOfPaginationReached && items.itemCount == 0) {
-            item {
-                emptyContent()
-            }
+            emptyContent()
         }
 
-        items(items) { image ->
-            if (image != null) {
-                MarsImageComposable(
-                    marsImage = image,
-                    onClick = { onItemClick(image) },
-                    onFavoriteClick = { onFavoriteClick(image) })
-            } else
-                Image(
-                    painter = painterResource(id = R.drawable.img_placeholder),
-                    contentDescription = ""
-                )
-        }
+        val spacedBy = Arrangement.spacedBy(16.dp)
+        LazyVerticalStaggeredGrid(
+            modifier = modifier.weight(1f),
+            contentPadding = PaddingValues(16.dp),
+            columns = if (gridView) {
+                StaggeredGridCells.Adaptive(150.dp)
+            } else {
+                StaggeredGridCells.Fixed(1)
+            },
+            verticalArrangement = spacedBy,
+            horizontalArrangement = spacedBy,
+            content = {
+                items(items.itemCount) {
+                    val image = items[it]
+                    if (image != null) {
+                        MarsImageComposable(
+                            marsImage = image,
+                            onClick = { onItemClick(image) },
+                            onFavoriteClick = { onFavoriteClick(image) })
+                    } else
+                        Image(
+                            painter = painterResource(id = R.drawable.img_placeholder),
+                            contentDescription = ""
+                        )
+                }
+            }
+        )
 
         if (items.loadState.append == LoadState.Loading) {
-            item {
-                CircularProgressIndicator(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .wrapContentWidth(Alignment.CenterHorizontally)
-                )
-            }
+            LinearProgressIndicator(modifier = Modifier.fillMaxWidth())
         }
-    })
+    }
 }
